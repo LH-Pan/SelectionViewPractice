eb16bfe502d36aca56894d71f1dfadd8129c435e
潘立祥 <panlishiang@panlixiangde-MacBook-Pro.local>
2019-08-19 11:01:11 +0800

    create SelectionView project

---
+++ SelectionViewAssignment/SelectionViewProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  SelectionViewProtocol.swift
+//  SelectionViewAssignment
+//
+//  Created by 潘立祥 on 2019/8/19.
+//  Copyright © 2019 PanLiHsiang. All rights reserved.
+//
+
+import UIKit
+
+class SelectionView {
+    
+    
+}
---
+++ SelectionViewAssignment/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  SelectionViewAssignment
+//
+//  Created by 潘立祥 on 2019/8/19.
+//  Copyright © 2019 PanLiHsiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
